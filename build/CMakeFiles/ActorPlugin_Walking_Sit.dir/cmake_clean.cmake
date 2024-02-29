50901ed191f99d72fd24fadffba820f0d6b7204e
Juunghyeon <ahdtld54@korea.ac.kr>
2024-02-29 14:17:27 +0900

    2024-02-29

---
+++ build/CMakeFiles/ActorPlugin_Walking_Sit.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ActorPlugin_Walking_Sit.dir/src/ActorPlugin_Walking_Sit.cc.o"
+  "libActorPlugin_Walking_Sit.pdb"
+  "libActorPlugin_Walking_Sit.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ActorPlugin_Walking_Sit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
